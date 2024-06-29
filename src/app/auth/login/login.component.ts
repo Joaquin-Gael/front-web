5572e6179979b41a08359c6b48d38189ca595dcc
guzmanantonio867 <guzmanantonio867@gmail.com>
2024-06-29 01:08:17 +0000

    update:agrege font awesome

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, Router, RouterModule } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
